7cdd63819591ac88c4ddb56818388f029d2cfccc
Егор Сяськов <egor.record@yandex.ru>
2021-11-08 17:32:06 +0300

    Update Button.jsx
    bub))

--- src/components/Button.jsx
+++ src/components/Button.jsx
@@ -2,7 +2,7 @@ import React from 'react';
 import PropTypes from 'prop-types';
 import classNames from 'classnames';
 
-const Button = ({ onClick, className, outline, children }) => {
+const Bub = ({ onClick, className, outline, children }) => {
   return (
     <button
       onClick={onClick}
